4aaf0fa0cb912b30f73ad39f2d896935bb2e4d85
A.Lejeune <acelyalejeune@gmail.com>
2026-08-28 15:38:50 +0200

    Import ReplyComposer into ticket detail

--- src/components/TicketDetail/TicketDetail.tsx
+++ src/components/TicketDetail/TicketDetail.tsx
@@ -1,5 +1,6 @@
 import './TicketDetail.scss'
 import type { Ticket } from '../TicketList/TicketList.types'
+import ReplyComposer from '../ReplyComposer/ReplyComposer'
 
 type TicketDetailProps = {
     ticket: Ticket
