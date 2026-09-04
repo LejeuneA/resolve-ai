c3c2ad253d01f24cd29d7ccd4ab8421d6a49f1c5
A.Lejeune <acelyalejeune@gmail.com>
2026-09-04 16:06:26 +0200

    Move ticket data into typed state

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import Sidebar from './components/Sidebar/Sidebar'
 import TicketList from './components/TicketList/TicketList'
 import TicketDetail from './components/TicketDetail/TicketDetail'
 import { tickets } from './data/tickets'
+import type { Ticket } from './components/TicketList/TicketList.types'
 
 function App() {
   const [selectedTicketId, setSelectedTicketId] = useState('ticket-001')
 
+  const [ticketdata, setTicketData] = useState<Ticket[]>([])
+
+
   const selectedTicket =
-    tickets.find((ticket) => ticket.id === selectedTicketId) ?? tickets[0]
+    ticketdata.find((ticket) => ticket.id === selectedTicketId) ?? tickets[0]
 
   function handleSubmitReply(reply: string) {
     console.log(reply)
